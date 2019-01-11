a4550715215b671bba54f4fef6af7175061749eb
jayanti18 <jayantirajesh@yahoo.com>
2019-01-11 14:04:59 -0500

    committing project

---
+++ Amazing.java
@@ -0,0 +1,10 @@
+
+public class Amazing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Jayanti");
+		System.out.println("I am building this project all on my own!");
+	}
+
+}
